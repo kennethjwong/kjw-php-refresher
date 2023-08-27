0232cb5ac80123a2fbc9abd4bd3769b099f9a46a
Kenneth <14003738+kennethjwong@users.noreply.github.com>
2023-08-26 23:32:46 -0700

    init

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'database' => [
+        'host' => '127.0.0.1',
+        'port' => 3306,
+        'dbname' => 'apps',
+        'charset' => 'utf8mb4',
+        'user' => 'root',
+        'password' => 'pass',
+    ],
+
+    //
+];
